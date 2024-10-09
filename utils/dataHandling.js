ce339f683653813dd66effe885e542e232800570
Daniel <daniel.lopez46@alu.uclm.es>
2024-10-09 11:33:50 +0200

    feat: processed rest of data

--- utils/dataHandling.js
+++ utils/dataHandling.js
@@ -11,13 +11,12 @@ const getTankTemperaturesData = (rawData) => {
     const date = new Date(lastObject.timestamp * 1000);
     const readableDate = date.toLocaleString();
 
-
     const result = {
       readableDate: readableDate,
       submerged_temperature: lastObject.fields.submerged_temperature,
       surface_temperature: lastObject.fields.surface_temperature,
       over_surface_temperature: lastObject.fields.over_surface_temperature,
-    }
+    };
 
     console.log("Last object ", lastObject);
     console.log("Result ", result);
@@ -79,11 +78,78 @@ const getMilkQuantityData = (rawData) => {
 };
 
 const getAirQualityData = (rawData) => {
+  const dataObject = JSON.parse(rawData);
+
+  let lastObject = dataObject[dataObject.length - 1];
+
+  const date = new Date(lastObject.timestamp * 1000);
+  const readableDate = date.toLocaleString();
+
   return rawData;
 };
 
 const getWeightData = (rawData) => {
-  return rawData;
+  const dataObject = JSON.parse(rawData);
+
+  let lastObject = dataObject[dataObject.length - 1];
+
+  const date = new Date(lastObject.timestamp * 1000);
+  const readableDate = date.toLocaleString();
+
+  const result = {
+    readableDate: readableDate,
+    weight: lastObject.fields.value,
+  };
+
+  return result;
+};
+
+const getMagneticSwitchData = (rawData) => {
+  const dataObject = JSON.parse(rawData);
+
+  let lastObject = dataObject[dataObject.length - 1];
+
+  const date = new Date(lastObject.timestamp * 1000);
+  const readableDate = date.toLocaleString();
+  
+  const result = {
+    readableDate: readableDate,
+    status: lastObject.fields.state,
+  };
+
+  return result;
+};
+
+const getEncoderData = (rawData) => {
+  const dataObject = JSON.parse(rawData);
+
+  let lastObject = dataObject[dataObject.length - 1];
+
+  const date = new Date(lastObject.timestamp * 1000);
+  const readableDate = date.toLocaleString();
+
+  const result = {
+    readableDate: readableDate,
+    value: lastObject.fields.rpm,
+  };
+
+  return result;
+};
+
+const getBoardTemperatureData = (rawData) => {
+  const dataObject = JSON.parse(rawData);
+
+  let lastObject = dataObject[dataObject.length - 1];
+
+  const date = new Date(lastObject.timestamp * 1000);
+  const readableDate = date.toLocaleString();
+
+  const result = {
+    readableDate: readableDate,
+    temperature: lastObject.fields.temperature,
+  };
+  
+  return result;
 };
 
 const processData = (topic, rawData) => {
@@ -102,15 +168,27 @@ const processData = (topic, rawData) => {
       processedData = getMilkQuantityData(rawData);
       break;
     case topics[3]:
-      console.log(`Received ${topics[3] + topic} `);
+      console.log(`Received ${topic} `);
       processedData = getAirQualityData(rawData);
       break;
     case topics[4]:
-      console.log(`Received ${topics[4] + topic} `);
+      console.log(`Received ${topic} `);
       processedData = getWeightData(rawData);
       break;
     case topics[5]:
+      console.log(`Received ${topic} `);
+      processedData = getMagneticSwitchData(rawData);
+    case topics[6]:
+      console.log(`Received ${topic} `);
+      processedData = getEncoderData(rawData);
+    case topics[7]:
+      console.log(`Received ${topic} `);
+      processedData = getBoardTemperatureData(rawData);
+    case topics[8]:
+      console.log(`Received ${topic} `);
+      processedData = getBoardStatusData(rawData);
     default:
+      console.log("Topic not found");
   }
 
   return processedData;
